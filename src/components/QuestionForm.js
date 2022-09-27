8ee49ecbaf7886292f8b6eb30cb878888e40a3a5
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2022-09-27 13:49:57 +0300

    Complete assignment

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function QuestionForm(props) {
   const [formData, setFormData] = useState({
@@ -9,7 +9,7 @@ function QuestionForm(props) {
     answer4: "",
     correctIndex: 0,
   });
-
+ 
   function handleChange(event) {
     setFormData({
       ...formData,
@@ -19,7 +19,23 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    // console.log(formData);
+    fetch("http://localhost:4000/questions",{
+    method:"POST",
+    headers :{
+      "Content-Type": "application/json",
+    },
+    body:JSON.stringify({
+      prompt:formData.prompt,
+      answers:[
+        formData.answer1,
+        formData.answer2,
+        formData.answer3,
+        formData.answer4
+      ],
+      correctIndex:parseInt(formData.correctIndex),
+    }),
+  })
   }
 
   return (
